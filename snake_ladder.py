c463b427b9a18911659c923bfaf9c1b6e4b64ecd
rishabhkjain0 <rishabhkjain0@gamil.com>
2020-08-23 22:25:45 +0530

    adding pytests

--- snake_ladder.py
+++ snake_ladder.py
@@ -4,15 +4,15 @@ import random
 class Input:
 
     def input_turns(self):
-        turns = input("Enter no of turns to play e.g. 10 (max 100)")
-        if turns.isdigit() and int(turns) <= 100:
-            return turns
-        else:
-            print("Please enter valid integer for turns")
+        turns = input("Enter no of turns to play e.g. 10 (max 100):")
+        try:
+            return int(turns)
+        except:
+            print("Please enter valid integer for turns:")
             self.input_turns()
 
     def input_dice_type(self):
-        dice_type = input("select dice 'N' for normal, 'C' for Crooked (gives even numbers)")
+        dice_type = input("select dice N for normal, C for Crooked:")
         if dice_type in ('N', 'C'):
             return dice_type
         else:
@@ -20,32 +20,40 @@ class Input:
             self.input_dice_type()
 
     def input_snake_positions(self):
-        snake_positions = input("Enter snake positions (start, end) comma separated e.g. (31,8), (40,12), (58, 20), (65, 30)")
-        start_end_positions = snake_positions.split(",")
-        if all(isinstance(eval(position), tuple) for position in start_end_positions):
-            return snake_positions
-        else:
-            print("Please input valid snake positions in format mentioned e.g. (31,8), (40,12)")
+        snake_positions = input("Enter snake positions (start, end) comma separated e.g. (31,8), (40,12):")
+        try:
+            start_end_positions = eval(snake_positions)
+            for position in start_end_positions:
+                if isinstance(position, tuple) and (position[0] > position[1]) and int(position[0]) < 100:
+                    return eval(snake_positions)
+                else:
+                    print("Please input valid snake positions in format mentioned e.g. [(31,8), (40,12)]")
+                    self.input_snake_positions()
+        except:
+            print("Please input valid snake positions in format mentioned e.g. [(31,8), (40,12)] not exceeding 99")
             self.input_snake_positions()
-
 
 
 class SnakeLadder:
 
     def __init__(self, snake_positions):
         self.player_position = 1
+        print(snake_positions)
         self.snake_positions = {position[0]: position[1] for position in snake_positions}
 
     @staticmethod
     def get_dice_value(dice_type):
         return random.randrange(1, 6) if dice_type == 'N' else random.choice([2, 4, 6])
 
-    def start_game(self, turns, dice_type, snake_positions):
+    def update_player_position(self, dice_value):
+        self.player_position += dice_value
+        if self.snake_positions.get(self.player_position):
+            self.player_position = self.snake_positions.get(self.player_position)
+
+    def start_game(self, turns, dice_type):
         while turns > 0:
             dice_value = SnakeLadder.get_dice_value(dice_type)
-            self.player_position += dice_value
-            if self.snake_positions.get(self.player_position):
-                self.player_position = self.snake_positions.get(self.player_position)
+            self.update_player_position(dice_value)
             print("Player moved to:" + str(self.player_position))
             turns -= 1
         print("Game end!!! Exit")
@@ -64,3 +72,33 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+def test_if_player_position_initialized():
+    self = SnakeLadder(snake_positions=[(31, 5)])
+    assert self.player_position == 1
+
+
+def test_player_position_if_snake_present():
+    turns = 6
+    self = SnakeLadder(snake_positions=[(31, 5)])
+    while turns > 0:
+        dice_value = 5
+        self.update_player_position(dice_value)
+        turns -= 1
+    assert self.player_position == 5
+
+
+def test_player_position_if_snake_absent():
+    turns = 6
+    self = SnakeLadder(snake_positions=[(20, 5)])
+    while turns > 0:
+        dice_value = 4
+        self.update_player_position(dice_value)
+        turns -= 1
+    assert self.player_position == 25
+
+
+def test_crooked_dice_value():
+    assert SnakeLadder.get_dice_value('C') % 2 == 0
+
